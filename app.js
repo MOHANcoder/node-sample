d64c1c5606fe860b5c7704c5ed278b70f3afb3b1
MOHANcoder <mohan235689@gmail.com>
2022-12-31 00:15:54 +0530

    First Commit !

---
+++ app.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const { readFileSync, writeFileSync } = require("fs");
+// // console.log(readFileSync("./sample.js","utf-8"));
+// // writeFileSync("./output.txt",)
+
+// const http = require("http")
+// const index = readFileSync('C:\\Users\\lenovo\\Desktop\\public\\index.html', 'utf8');
+// const server = http.createServer((req, res) => {
+//     if (req.url == '/') {
+//         res.write(index);
+//     } else {
+//         res.write(`
+//     <h1>Hi friends!</h1>
+//     `)
+//     }
+//     //res.write()
+//     res.end()
+// })
+
+// server.listen(1000)
+
+// console.log(require("./sample.js").info);
---
+++ sample.js
@@ -0,0 +1,9 @@
+// console.log(process.argv);
+// console.log(process.env[process.argv[2]]);
+// console.log(module);
+const info = {
+    name : "S.Mohan",
+    age : 19
+};
+
+module.exports.info = info;
